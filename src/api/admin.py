37a212e687babfd2892b1c51548f5f685c54c74a
Ryan Agdassi <113940065+ragdassi@users.noreply.github.com>
2024-04-15 11:38:41 -0700

    admin resetting functionality for green/red/blue potions

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+from src import database as db
+import sqlalchemy
 
 router = APIRouter(
     prefix="/admin",
@@ -14,5 +16,20 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+
+    with db.engine.begin() as connection:
+        # Set gold back to 100
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = :gold"),
+                    {"gold": 100})
+        #set inventory back to 0
+
+        # num potions back to 0
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = :num_green_potions, num_blue_potions = :num_blue_potions, num_red_potions = :num_red_potions"),
+                    {"num_green_potions": 0, "num_blue_potions": 0, "num_red_potions": 0})
+        
+        # mls back to 0
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET millileters = :millileters, green_ml = :green_ml, red_ml = :red_ml, blue_ml = :blue_ml"),
+                    {"millileters": 0, "green_ml": 0, "red_ml": 0,  "blue_ml": 0})
+        
     return "OK"
 
